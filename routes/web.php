7445f65993333e96f1ba01fdc7fc0ce2495e0043
Stepan <kruziks.06@spst.eu>
2025-10-07 17:58:55 +0200

    Obrazek na celou plochu, oříznutí obrázku

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ImageController;
+use Illuminate\Http\Request;
+
 
 Route::get('/', [ProjectController::class, 'index'])->name('home');
-Route::post('/upload', [ProjectController::class, 'upload'])->name('upload');
-Route::post('/create-blank', [ProjectController::class, 'createBlank'])->name('createBlank');
+Route::post('/upload', [ImageController::class, 'upload'])->name('upload');
+Route::post('/createBlank', [ImageController::class, 'createBlank'])->name('createBlank');
 
-Route::get('/editor/{filename}', [ProjectController::class, 'editor'])->name('editor');
+Route::get('/editor', function() {
+    $imagePath = 'storage/' . request()->get('path');
+    return view('editor', compact('imagePath'));
+})->name('editor');
