cc39e3b75d5d17667e2a32b44fa563ef4c89c362
Stepan <kruziks.06@spst.eu>
2025-12-17 09:16:33 +0100

    ikony obrazců

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}"> <!-- Aby fungovalo ukládání na server -->
     <title>Home Page</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body class="h-full">
